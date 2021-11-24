9938118a828792a6fe2b8a2935e3db096d982d37
alexisnov <novosselski@gmail.com>
2021-11-24 15:20:09 +0300

    +

--- imgproc.cpp
+++ imgproc.cpp
@@ -1,6 +1,24 @@
 #include "imgproc.h"
 
-ImgProc::ImgProc()
+ImgProc::ImgProc(QString name,QString path):name(name),path(path)
 {
+    //Загрузка картинки
+    bool res = image.load(path);
+    if(res){
+        //TODO: обработка ошибки загрузки изображения
+        this->deleteLater();
+    }
+}
 
+void ImgProc::run(){
+    //Обработка картинки
+    if(!image.isNull()){
+        int k = 2;
+        //Масштабирование
+        image = image.scaled(QSize(image.width()/k,image.height()/k));
+        //Сохранение
+        image.save(path);
+        //Сигнал готовности
+        emit ready(name);
+    }
 }
